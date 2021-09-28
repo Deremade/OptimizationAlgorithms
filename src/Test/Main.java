e397e50e4ea8da37a7d7e10d59cc3485c323f6bb
Deremade <drtimothy007@gmail.com>
2021-09-28 02:05:31 -0700

    Update Main.java
    cleared testing methods

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -10,42 +10,7 @@ import Solution.NumericAlgorithm;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		NumericAlgorithm<Double> na = new NumericAlgorithm<Double>(1, new LocalMaxTest<Double>(), 3, -5.0, 5.0, 1.0);
-		na.fillSolutions(5);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
-		na.iterations(15);
-		System.out.println(na.displaySolutions());
+		
 	}
 
 }
