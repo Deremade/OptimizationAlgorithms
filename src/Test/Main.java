0ee2f1649ffa8a2683b7a2547fc6a076d1e0ed3e
Deremade <drtimothy007@gmail.com>
2021-09-28 19:43:48 -0700

    Created Basic Selector
    -TopSurvivors : Returns the top few solutions and drops all non survivors

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,6 +1,10 @@
 package Test;
 
+import java.util.LinkedList;
+
 import Solution.NumericAlgorithm;
+import Solution.OptimizationSolution;
+import Solution.SelectionMethod;
 
 /**
  * The Main class for testing
@@ -10,6 +14,14 @@ import Solution.NumericAlgorithm;
 public class Main {
 
 	public static void main(String[] args) {
+		NumericAlgorithm<Integer> na = new NumericAlgorithm<Integer>(1, new CircleProblem<Integer>(), 3, 0, 25, 1);
+		
+		na.generateSolutions(10);
+		
+		SelectionMethod sm = SelectionMethod.topSurvivors(5);
+		System.out.println(na.displaySolutions());
+		sm.subjectToSelection(na.solutions());
+		System.out.println(na.displaySolutions());
 		
 	}
 
