0d6d804aa4e4711e13b454b53a620475ae4deb5c
Deremade <drtimothy007@gmail.com>
2021-09-30 00:50:34 -0700

    Update Main.java

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -14,14 +14,6 @@ import Solution.SelectionMethod;
 public class Main {
 
 	public static void main(String[] args) {
-		NumericAlgorithm<Integer> na = new NumericAlgorithm<Integer>(1, new CircleProblem<Integer>(), 3, 0, 25, 1);
-		
-		na.generateSolutions(10);
-		
-		SelectionMethod sm = SelectionMethod.topSurvivors(5);
-		System.out.println(na.displaySolutions());
-		sm.subjectToSelection(na.solutions());
-		System.out.println(na.displaySolutions());
 		
 	}
 
